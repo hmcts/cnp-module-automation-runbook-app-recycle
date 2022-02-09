4c25992af18bc0837c544a24f0b6aed84a427bb1
Christopher Pateman <christopher.pateman@version1.com>
2022-02-09 22:14:49 +0000

    Add all the parameters

--- runbook-schedule.tf
+++ runbook-schedule.tf
@@ -1,7 +1,24 @@
+data "azurerm_client_config" "current" {}
 locals {
+  source_managed_identity_id = var.source_managed_identity_id == "" ? data.azurerm_client_config.current.object_id : var.source_managed_identity_id
+  source_tenant_id           = data.azurerm_client_config.current.tenant_id
+
   today      = timestamp()
   start_date = formatdate("YYYY-MM-DD", timeadd(local.today, "24h"))
   start_time = "01:00:00"
+
+  parameters = {
+    servicePrincipalIdCollection = jsonencode(var.application_id_collection)
+    sourceTenantId               = local.source_tenant_id
+    sourceClientId               = local.source_managed_identity_id
+    targetTenantId               = var.target_tenant_id
+    targetApplicationId          = var.target_application_id
+    targetApplicationSecret      = var.target_application_secret
+    environment                  = var.environment
+    product                      = var.product
+    prefix                       = "auto-"
+    keyVaultName                 = var.key_vault_name
+  }
 }
 
 resource "azurerm_automation_schedule" "client_serects" {
@@ -21,4 +38,22 @@ resource "azurerm_automation_schedule" "client_serects_trigger_once" {
   frequency               = "OneTime"
   start_time              = formatdate("YYYY-MM-DD'T'hh:mm:ssZ", timeadd(local.today, "10m"))
   description             = "This is a one time trigger to automate the renewal and recycling of Client Secrects"
+}
+
+resource "azurerm_automation_job_schedule" "client_serects" {
+  resource_group_name     = var.resource_group_name
+  automation_account_name = var.automation_account_name
+  schedule_name           = azurerm_automation_schedule.client_serects.name
+  runbook_name            = azurerm_automation_runbook.client_serects.name
+  parameters              = local.parameters
+  depends_on              = [azurerm_automation_schedule.client_serects]
+}
+
+resource "azurerm_automation_job_schedule" "client_serects_trigger_once" {
+  resource_group_name     = var.resource_group_name
+  automation_account_name = var.automation_account_name
+  schedule_name           = azurerm_automation_schedule.client_serects_trigger_once.name
+  runbook_name            = azurerm_automation_runbook.client_serects.name
+  parameters              = local.parameters
+  depends_on              = [azurerm_automation_schedule.client_serects_trigger_once]
 }
