7a9c0eb8e3c5a8f8ccc3e0dffa4a0200d4a95339
Christopher Pateman <christopher.pateman@version1.com>
2022-02-09 14:29:52 +0000

    fix timing

--- runbook-schedule.tf
+++ runbook-schedule.tf
@@ -1,7 +1,7 @@
 locals {
   today      = timestamp()
   start_date = formatdate("YYYY-MM-DD", timeadd(local.today, "24h"))
-  start_time = "01:00:00Z"
+  start_time = "01:00:00"
 }
 
 resource "azurerm_automation_schedule" "client_serects" {
@@ -10,7 +10,7 @@ resource "azurerm_automation_schedule" "client_serects" {
   automation_account_name = var.automation_account_name
   frequency               = "Day"
   interval                = 1
-  start_time              = "${local.start_date}T${local.start_time}"
+  start_time              = "${local.start_date}'T'${local.start_time}Z"
   description             = "This is a schedule to automate the renewal and recycling of Client Secrects"
 }
 
