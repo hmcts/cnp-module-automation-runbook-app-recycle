25a30cc01592f511ccef0b3d3db82e5cb671ba87
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2023-04-27 14:58:06 +0100

    PUB-1993 - Fixed remove

--- client_secrets.ps1
+++ client_secrets.ps1
@@ -175,7 +175,7 @@ try {
             Write-Output "Expiry Date Range is $expiringRangeDate. It has $($expiringRangeDateObj.Days) day"
             if ($endDateObj.Days -lt 1) {
               Write-Output "$keyName has expired ($endDate). Removing Key"
-              Remove-AzADAppCredential -ObjectId $appId -KeyId $keyId -Confirm:$false -DefaultProfile $targetContext
+              Remove-AzADAppCredential -ObjectId $objectId -KeyId $keyId -DefaultProfile $targetContext
             }
             elseif ($expiringRangeDateObj.Days -lt $expiringRangeDays) {
               Write-Output "$keyName will expire within $expiringRangeDays."           
